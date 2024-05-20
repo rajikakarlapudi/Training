e5692876ba12ff2dd255fa58151048b2f758ae36
Kakarlapudi Rajeswari <127588101+rajikakarlapudi@users.noreply.github.com>
2024-05-20 21:47:23 +0530

    Create airthop.py

---
+++ airthop.py
@@ -0,0 +1,27 @@
+"""Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+Example
+
+
+Print the following:
+
+8
+-2
+15
+Input Format
+
+The first line contains the first integer, .
+The second line contains the second integer, .
+"""
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
